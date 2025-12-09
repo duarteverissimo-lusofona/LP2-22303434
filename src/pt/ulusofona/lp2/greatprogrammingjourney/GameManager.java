1156c234d9bf8d37ce089571ac7f34bc288bd5df
Duarte-Verissimo <142510215+Duarte-Verissimo@users.noreply.github.com>
2025-12-09 19:26:18 +0000

    correcao final(é desta)

--- src/pt/ulusofona/lp2/greatprogrammingjourney/GameManager.java
+++ src/pt/ulusofona/lp2/greatprogrammingjourney/GameManager.java
@@ -680,8 +680,6 @@ public class GameManager {
         // REGRA: Jogadores PRESOS ou DERROTADOS não podem jogar
         // O turno passa e conta como uma jogada
         if (jogadorAtual.getEstado() == Estado.PRESO || jogadorAtual.getEstado() == Estado.DERROTADO) {
-            numTurnos++;  // Conta como um turno
-            jogadorAtualIndex = getNextPlayer();  // Passa para o próximo
             return false;
         }
 
